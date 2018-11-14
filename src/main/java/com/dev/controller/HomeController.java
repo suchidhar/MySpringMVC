9f1eed7b1df44f7f1f87b1dd310ee50857510dfb
Suchismita Dhar <Suchismita Dhar@Suchismita>
2018-11-14 12:44:35 +0530

    dispatcher and application context updated. HomeController updated.

--- src/main/java/com/dev/controller/HomeController.java
+++ src/main/java/com/dev/controller/HomeController.java
@@ -1,5 +1,16 @@
 package com.dev.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/controller")
 public class HomeController {
+	
+	@RequestMapping(value="/home",method=RequestMethod.GET)
+	public String hello() {
+		return "Home";
+	}
 	
 }
